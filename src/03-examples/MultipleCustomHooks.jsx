334279340d514006b7217227131d02c53f07ec3b
Erick Dejo Vicente <61488294+mapachefurioso@users.noreply.github.com>
2023-11-01 22:27:48 -0500

    añadiendo las apis

---
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -0,0 +1,32 @@
+import { useCounter,useFetch } from "../hooks";
+import { BreakingQuote } from "./BreakingQuote";
+import { LoadingQuote } from "./LoadingQuote";
+
+export const MultipleCustomHooks = () => {
+  const {counter,increment} = useCounter(1);
+  const { data, isLoading, hasError } = useFetch(
+    `https://api.breakingbadquotes.xyz/v1/quotes/${counter}`
+  );
+    console.log(data);
+    const {author,quote} = !!data && data[0];
+
+  return (
+    <>
+      <h1>BreakingBad Quotes</h1>
+      <hr />
+
+      {isLoading ? (
+        <LoadingQuote></LoadingQuote>
+      ) : (
+        <BreakingQuote author={author} quote={quote}></BreakingQuote>
+      )}
+      
+      
+
+      <button className="btn btn-primary" disabled={isLoading} onClick={()=>increment()}>
+        Next quote
+      </button>
+    </>
+
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { SimpleForm } from "./02-useEffect/SimpleForm";
 import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
+import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <FormWithCustomHook />
-  </React.StrictMode>
+  <>
+    <MultipleCustomHooks />
+  </>
 );
